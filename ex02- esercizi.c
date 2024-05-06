0b9d49be791f994e887ff6919fb0c6d165dd9fea
gretaaaaaaaaaa <gretasabia@gmail.com>
2024-05-06 14:59:19 +0000

    !

--- ex02- esercizi.c
+++ ex02- esercizi.c
@@ -6,7 +6,7 @@ int main ()
     float b= 3.44532 g= 2.228819
     double c= 2.7367136 k= 9.28838
     a= 5 v= 3
-    printf ( "somma:")
+    printf ( "somma: %d\n %f" a*b)
     printf ("differenza: ")
     printf ("divisione: ")
     printf ("moltiplicazione:")
