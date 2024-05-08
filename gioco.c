298f9d12c10b867643a73b562b4ac97284dd1be7
gretaaaaaaaaaa <gretasabia@gmail.com>
2024-05-08 23:34:27 +0200

    Update gioco.c

--- gioco.c
+++ gioco.c
@@ -3,24 +3,50 @@
 int main()
 {
     int voto;
-    int min=6;
+    int min = 6;
+    int risposta;
+    int materia;
+    int risultato;
 
-scanf("%s", &voto);
  printf("inserisci nome utente:");
     char  nomeutente[ 50 ];
 scanf("%s", nomeutente);
 
-printf("quanto prenderai all'esame di matematica?:");
-    char  voto[ 50 ];
-scanf("%d", &voto);
-if(voto<min)
+printf("sei pronto per gli esami(si/no)?\n");
+scanf("%s", risposta);
+if( risposta = si )
+    {
+    printf("allora cominciamo\n");
+    printf("quanto prenderai all'esame di matematica?:");
+    scanf("%d", &voto);
+    if(voto < min)
+    {
+        printf("sei stato bocciato, ritenta l'anno prossimo");
+    }
+    else
+     {
+        printf("bravissimo, ora continua a studiare\n");
+        printf("quale materia scegli ora?");
+        scanf("%d", &materia);
+        printf("sei sicuro di voler procedere(si/no)?\n");
+            if( risposta= si )
+            {
+                printf("allora ci vediamo tra un'ora e mezza!\n");
+                printf("un'ora e mezza dopo...\n");
+                printf("com'è andata(bene/male)?\n");
+                scanf("%d" &risultato);
+                if
+                
+            }
+
+
+    }
+if(risposta= no)
 {
-    printf("sei tato bocciato, ritenta l'anno prossimo");
+    printf("e allora cosa aspetti? studia!!!");
 }
-if(voto>min)
- {
-    printf("bravissimo! sostieni i prossimi esami");
- }
+
+    
 
 
 
